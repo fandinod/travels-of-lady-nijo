10c90a64f413a12e27d54e13480b177516f28fa4
fandinod <32309230+fandinod@users.noreply.github.com>
2022-08-24 00:27:01 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://fandinod.github.io/travels-of-lady-nijo/#2';
+var googleDocURL = 'https://fandinod.github.io/travels-of-lady-nijo/';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCIVfkQv7QXSmuuIoJkQBnWiYteGxso2Xk';
